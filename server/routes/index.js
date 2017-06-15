1adf9b95265b3a9cb0dd947c35c163c9e3931935
Patrick Kovach-Long <PatrickKLong@gmail.com>
2017-06-15 12:21:59 -0400

    Add routes and controller for Todos

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+const todosController = require('../controllers').todos
+
+module.exports = app => {
+  app.get('/api', (req, res) => {
+    res.status(200).send({
+      message: 'Todos API'
+    })
+  })
+
+  app.post('/api/todos', todosController.create)
+  app.get('/api/todos', todosController.index)
+}
